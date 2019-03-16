235e84f07f1a99885b46b892b3fc2ac97e77e937
FrankV <frank@sabertechnology.com>
2019-03-16 14:12:17 -0400

    Added handlebars functionality

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,38 +1,12 @@
-// var db = require("../models");
-
-// module.exports = function(app) {
-//   // Load index page
-//   app.get("/", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.render("index", {
-//         msg: "Welcome!",
-//         examples: dbExamples
-//       });
-//     });
-//   });
-
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
-
-//   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.render("404");
-//   });
-// };
 
 
 module.exports = function(app) {
 
  
-
 app.get("/home", function(req, res) {
 
+
+  //Next 6 lines are just test data
   var foxUser = [
     {
       userID: "jsmith161",
@@ -41,8 +15,7 @@ app.get("/home", function(req, res) {
   ];
   // Handlebars requires an object to be sent to the handlebars file.
   // User must be set as an object
-  // var currentUser = foxUser[1].name;
-  // console.log(currentUser);
+
   //Send the user object to the index.handlebars file.
   res.render("home", {
     title: "Secure Fox",
