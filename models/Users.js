6f7131db924b49b354e260e9d1662b7fcc891e01
farhan afzal <koolfani@gmail.com>
2019-03-16 18:48:32 -0400

    bcrypt working

--- models/Users.js
+++ models/Users.js
@@ -2,8 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var users_tables = sequelize.define("users_tables", {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    password2: DataTypes.STRING,
+    password: DataTypes.STRING
   });
   return users_tables;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,18 @@
 var db = require("../models");
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    
+      res.render("index")
+    
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+      res.render("example")
   });
 
   // Route for signup page
@@ -32,7 +27,41 @@ module.exports = function(app) {
     
   });
 
-  
+  //register form post
+  app.post('/signup',(req,res)=>{
+    let errors =[];
+    if(req.body.password != req.body.password2){
+      errors.push({text:'Password do not match!'})
+    }
+    if(req.body.password.length < 4){
+      errors.push({text:'Password must be atleast 4 characters'})
+    }
+    //so the form doesnt clear
+    if(errors.length > 0){
+      res.render('signup',{
+        errors: errors,
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        password2:req.body.password2
+      })
+    } else{
+      const newUser = {
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+      }
+      //hashing the password so it get encrysted before it is saved in the database
+      bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+          if(err) throw err;
+          newUser.password = hash;
+          db.users_tables.create(newUser);
+          res.send('done');
+        });
+      });
+    }
+});
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
