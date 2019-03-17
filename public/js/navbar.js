6db7556f28981f5a191be0dd11b40c4987662ea2
Jose L Saboya Rosales <saboya@Joses-MBP.home>
2019-03-17 10:03:28 -0400

    Almost done, currently working on arrow back to top. Animations are still pending such as things appear at keypoints. Got the sticky navigation to work upon scroll. Would like to add the href info for the sign in and sign up page.

---
+++ public/js/navbar.js
@@ -0,0 +1,27 @@
+// responsive navbar (changes color on scrolling)
+$(window).scroll(function () {
+  let height = $(".navbar").height();
+  let scrollTop = $(window).scrollTop();
+  if (scrollTop >= height + 200) {
+    // makes the navbar solid
+    $(".navbar").addClass("nav-solid");
+  } else {
+    // restores the bg transparency
+    $(".navbar").removeClass("nav-solid");
+  }
+});
+
+/* After a certain point on the page When scrolling down, hide the navbar. When scrolling up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let scrollTop = $(window).scrollTop();
+  let currentScrollPos = window.pageYOffset;
+  if (scrollTop >= 790) {
+    if (prevScrollpos > currentScrollPos) {
+      $(".navbar").css("top", "0");
+    } else {
+      $(".navbar").css("top", "-8em") ;
+    }
+    prevScrollpos = currentScrollPos;
+  } 
+}
