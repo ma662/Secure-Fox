06f85f7d3f2c145d86309bf60f0238219cbe7234
Chlora <ma662@njit.edu>
2019-03-18 01:19:48 -0400

    Fix routes formatting

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,9 @@ var db = require("../models");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
-
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     return res.render("index");
   });
 
@@ -17,41 +16,26 @@ module.exports = function (app) {
     return res.render("login");
   });
 
-  // app.get("/text",(req,res) => {
-  //   res.render("text");
-
-  // });
-
-  //log in post 
-  // app.post("/login",(req,res,next) =>{
-
-  //     passport.authenticate("local",{
-  //     successRedirect:"/text",
-  //     failureRedirect:"/login",
-  //     failureFlash:true
-  //     })(req,res,next);
-
-  // });
-  app.post("/login",
-    passport.authenticate("local",
-      { failureRedirect: "/login", failureFlash: "Bad Login!" }),
-
-    function (req, res, next) {
-
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: "Bad Login!"
+    }),
+    function(req, res) {
       res.render("home");
-    });
-
-
+    }
+  );
 
   //register form post
   app.post("/signup", (req, res) => {
     let errors = [];
     let msg = [];
-    if (req.body.password != req.body.password2) {
-      errors.push({ text: "Password do not match!" })
+    if (req.body.password !== req.body.password2) {
+      errors.push({ text: "Password do not match!" });
     }
     if (req.body.password.length < 4) {
-      errors.push({ text: "Password must be atleast 4 characters" })
+      errors.push({ text: "Password must be atleast 4 characters" });
     }
     //so the form doesnt clear
     if (errors.length > 0) {
@@ -61,42 +45,40 @@ module.exports = function (app) {
         email: req.body.email,
         password: req.body.password,
         password2: req.body.password2
-      })
+      });
     } else {
       //check if email already exists in the db
-      db.users_tables.findOne({ where: { email: req.body.email } })
-        .then((users_tables) => {
-          if (users_tables) {
-            errors.push({ text: "Email already Registered! Please Login." });
-            res.render("signup", {
-              errors: errors
-            });
-            //if email doesnt exist, create new account
-          } else {
-            const newUser = {
-              name: req.body.name,
-              email: req.body.email,
-              password: req.body.password
-            };
-            //hashing the password so it get encrysted before it is saved in the database
-            bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) {
-                  throw err;
-                }
-
-                newUser.password = hash;
-                db.users_tables.create(newUser);
-                msg.push({
-                  text: "Thanks for Signing Up.Please Log in to Continue."
-                });
-                res.render("login", {
-                  msg: msg
-                });
+      db.User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          errors.push({ text: "Email already Registered! Please Login." });
+          res.render("signup", {
+            errors: errors
+          });
+          //if email doesnt exist, create new account
+        } else {
+          const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+          };
+          //hashing the password so it get encrysted before it is saved in the database
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) {
+                throw err;
+              }
+              newUser.password = hash;
+              db.users_tables.create(newUser);
+              msg.push({
+                text: "Thanks for Signing Up.Please Log in to Continue."
+              });
+              res.render("login", {
+                msg: msg
               });
             });
-          }
-        });
+          });
+        }
+      });
     }
   });
 
