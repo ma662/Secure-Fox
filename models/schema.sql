1b8c3317cb387fa0ad7a0c7e7435e1d740ae4185
ma662 <ma662@njit.edu>
2019-03-20 16:08:41 -0400

    Forgot to modify

--- models/schema.sql
+++ models/schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Database Creation
 DROP DATABASE IF EXISTS journal_db;
 CREATE DATABASE journal_db;
@@ -6,9 +5,3 @@ Use journal_db;
 
 DROP DATABASE IF EXISTS test_db;
 CREATE DATABASE test_db;
-=======
-DROP DATABASE IF EXISTS journal_db;
-CREATE DATABASE journal_db;
-
-USE journal_db;
->>>>>>> c79fd102d9d4754b3944311e6a27e277644ccf89
